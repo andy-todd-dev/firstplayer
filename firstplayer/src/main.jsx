ad32a1380807223fb52bb050d74780e475523503
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2023-05-04 14:00:18 +0100

    Toggle debug behind button (#5)

--- firstplayer/src/main.jsx
+++ firstplayer/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { IconContext } from "react-icons";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <IconContext.Provider value={{ size: "1em", className: "react-icon" }}>
+      <App />
+    </IconContext.Provider>
+  </React.StrictMode>
+);
