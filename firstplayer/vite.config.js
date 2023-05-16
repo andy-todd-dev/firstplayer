1f79bf3169500a90dbd64d1521e14a5c7a43b9b4
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2023-05-16 12:41:04 +0100

    Pwa manifest (#12)
    * Add PWA requirements

--- firstplayer/vite.config.js
+++ firstplayer/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 import pwaManifest from "./pwa_manifest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(pwaManifest)],
+  plugins: [react(), pwaManifest],
+  server: {
+    cors: true,
+  },
 });
