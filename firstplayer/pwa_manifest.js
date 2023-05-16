f06c934eb038c7fb44a03c9bbf2a9b9d7125e996
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2023-05-16 11:36:07 +0000

    Add PWA requirements

--- firstplayer/pwa_manifest.js
+++ firstplayer/pwa_manifest.js
@@ -1,15 +1,34 @@
-const pwaManifest = {
+import { VitePWA } from "vite-plugin-pwa";
+
+const pwaManifest = VitePWA({
   registerType: "autoUpdate",
   manifest: {
     name: "First Player",
-    short_name: "First Player",
+    short_name: "FP",
     description: "An app to pick a first player for a board game",
     start_url: ".",
     display: "standalone",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   },
-  devOptions: {
-    enabled: true,
-  },
-};
+});
 
 export default pwaManifest;
